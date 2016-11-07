816319fd89f0e7f77dc5c7779b2d4309dd828284
Magnus Hovik <maghovk@gmail.com>
2016-11-07 17:50:50 +0100

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	SLIT-Application/dist/SLIT-Application.jar
    #	SLIT-Application/dist/SLIT-ApplicationClient.jar
    #	SLIT-Application/dist/SLIT-ApplicationClient/SLIT-Application.jar
    #
    SLIT-Application/dist/SLIT-ApplicationClient/SLIT-ApplicationClient.jar
    #	SLIT-Interfaces/build/built-jar.properties

--- SLIT-Application/src/java/SLIT/FXMLVelgFagController.java
+++ SLIT-Application/src/java/SLIT/FXMLVelgFagController.java
@@ -5,9 +5,17 @@
  */
 package SLIT;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -23,5 +31,16 @@ public class FXMLVelgFagController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void velgIS109Event(ActionEvent event) throws IOException {
+         Parent log_page_parent = FXMLLoader.load(getClass().getResource("FXMLIS109.fxml"));
+            Scene log_page_scene = new Scene(log_page_parent);
+            Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            app_stage.setScene(log_page_scene);
+            app_stage.show();
+    }
     
+    
+ 
 }
