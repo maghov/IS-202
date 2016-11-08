66fc8b3a3261423c4d944286625a2f41326f91fc
Mohammad Hussain <mo.huss2@gmail.com>
2016-11-08 18:20:28 +0100

    bugfix

--- SLIT-Server/SLIT-Server-ejb/build/generated-sources/ap-source-output/EntityClasses/Users_.java
+++ SLIT-Server/SLIT-Server-ejb/build/generated-sources/ap-source-output/EntityClasses/Users_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-08T13:37:26")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-08T18:11:52")
 @StaticMetamodel(Users.class)
 public class Users_ { 
 
